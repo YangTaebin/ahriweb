4c943ff9d42e079939760c5354258c6b7a908643
20-069_taebin <ksa.yangtaebin@gmail.com>
2021-01-26 05:20:35 +0900

    no message

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
@@ -6,9 +7,21 @@ app = Flask(__name__)
 def main():
     return render_template('index.html',title="AHRIAHRI")
 
-@app.route('/us')
-def US():
-    return "FUCK World!"
+@app.route('/stock')
+def stock():
+    ahristock = pymysql.connect(
+        user="root",
+        passwd="taebin0408!",
+        host="localhost",
+        db="ahriweb"
+    )
+    ahristock_cursor = ahristock.cursor(pymysql.cursors.DictCursor)
+    sql="SELECT * FROM `ahristock`;"
+    ahristock_cursor.execute(sql)
+    ahristock_result = ahristock_cursor.fetchall()
+    ahristock_num = len(ahristock_result)
+    ahristock_now = int(ahristock_result[ahristock_num-1]['stock'])
+    return "현재 주가 : "+str(ahristock_now)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=80)
