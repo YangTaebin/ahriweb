c6e6a39369815510d48b003d6f4938b10ca76e5f
20-069_taebin <ksa.yangtaebin@gmail.com>
2021-01-26 11:38:02 +0900

    아리아리 주식의 주식값을 변화시킬 수 있는 페이지 제작

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import pymysql
 
 app = Flask(__name__)
@@ -26,9 +26,33 @@ def stock():
 
     return render_template("stock.html",title="아리아리 주식",ahristock=ahristock_now)
 
-@app.route('/setahristock')
+@app.route('/setstock')
 def setstock():
-    pass
+    return render_template("setstock.html",title="주식 설정")
+
+@app.route('/stock_change',methods=['POST'])
+def changing_p():
+    jong = request.form['jong']
+    value = request.form['value']
+    if jong=="ahri":
+        ahristock=pymysql.connect(
+            user="root",
+            passwd="taebin0408!",
+            db="ahriweb",
+            host="localhost"
+        )
+        ahristock_cursor = ahristock.cursor(pymysql.cursors.DictCursor)
+
+        sql="SELECT * FROM `ahristock`;"
+        ahristock_cursor.execute(sql)
+        ahristock_result = ahristock_cursor.fetchall()
+
+        ahristock_num = len(ahristock_result)
+
+        sql="insert into `ahristock` (num,stock) values ("+str(ahristock_num+1)+","+value+")"
+        ahristock_cursor.execute(sql)
+        ahristock.commit()
+        return render_template('stock_changing.html',title="변경 완료",jong=jong,value=value)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=80)
