d46a9dc8100670c93482bc5bc49a6e556b85c33d
20-069_taebin <ksa.yangtaebin@gmail.com>
2021-01-29 03:28:27 +0900

    no message

--- server/sub_func.py
+++ server/sub_func.py
@@ -37,3 +37,22 @@ def id_to_univ_id(id):
         if id == i['kakao_login_id']:
             return i['univ_id']
     return 0
+
+def already_univ(univ_id):
+    user = pymysql.connect(
+        user="root",
+        passwd="taebin0408!",
+        db="ahriweb_user",
+        host="localhost"
+    )
+
+    user_cursor = user.cursor(pymysql.cursors.DictCursor)
+
+    sql="SELECT * FROM `user`;"
+    user_cursor.execute(sql)
+    user_list = user_cursor.fetchall()
+
+    for i in user_list:
+        if univ_id==i['univ_id']:
+            return True
+    return False
